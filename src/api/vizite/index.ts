1c4ee8f8178ed7dd6bc907de105dbb041d764b8d
Inn-Keeper <codetilltheresnotomorrow@gmail.com>
2025-06-18 12:25:57 +0200

    URL tests

--- src/api/vizite/index.ts
+++ src/api/vizite/index.ts
@@ -1,4 +1,7 @@
 // @ts-nocheck
+if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
+  throw new Error('NEXT_PUBLIC_API_BASE_URL is not set! Please configure it in your environment variables.');
+}
 export * from './runtime';
 export * from './apis/index';
 export * from './models/index';
